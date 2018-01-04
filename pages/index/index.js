b91c09ae967c5c99fea3302fb16d361085957a69
darren <764190362@qq.com>
2018-01-04 15:25:30 +0800

    画出蛇头

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,6 +10,15 @@ var moveY = 0;
 var X = 0;
 var Y = 0;
 
+//蛇头的坐标
+var snakeHead = {
+  x:0,
+  y:0,
+  color: '#ff0000',
+  w: 20,
+  h:20
+};
+
 Page({
   canvasStart:function(e){
     startX = e.touches[0].x;
@@ -31,5 +40,27 @@ Page({
     } else if (Math.abs(Y) > Math.abs(X) && Y < 0) {
       console.log('moving top');
     }
+  },
+  onReady: function(){
+       //获取画布上的上下文
+       var context = wx.createContext();
+
+       function animate(){
+         //先设置
+          context.setFillStyle(snakeHead.color);
+          context.beginPath();
+          context.rect(snakeHead.x, snakeHead.y, snakeHead.w, snakeHead.h);
+          context.closePath();
+          context.fill();
+
+          //再画
+          wx.drawCanvas({
+            canvasId: "snakeCanvas",
+            actions: context.getActions()
+          });
+
+          requestAnimationFrame(animate);
+       }
+       animate();
   }
 })
